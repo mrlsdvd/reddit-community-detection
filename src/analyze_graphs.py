62cdf425387df65dec6219ecfc454dcb2e646b9b
mrlsdvd <mrlsdvd@stanford.edu>
2018-12-06 00:53:07 -0800

    Add skeleton code for other analysis functions

--- src/analyze_graphs.py
+++ src/analyze_graphs.py
@@ -79,3 +79,73 @@ def extract_topics_from_community(user_topic_graph, community, ratio_thresh=0.5)
         topic_ratio = float(topic_counts[topic]) / num_members
         if topic_ratio > ratio_thresh:
             topic_ratios.add((topic, topic_ratio))
+
+
+def community_topic_evolution(community_levels, user_topic_graph):
+    """
+    Analyzes the topic distribution in communities over levels (such as those
+    returned by Girvan-Newman community detection).
+
+    Arguments:
+        community_levels (list): List of tuples of list of nodes, where tuple at
+            position i consisists of lists of nodes representing the communities
+            after iteration i+1 of the Girvan-Newman method
+        user_topic_graph (nx.Graph): User-topic graph to link users to
+            their topics
+
+    Returns:
+        evolution (list): List of tuples of list of topics, where tuple at
+            position i consists of lists of topics corresponding to the
+            distribution of topics for each community after iteration i+1 of
+            the Girvan-Newman method
+    """
+    pass
+
+
+def sample_topics(topics, n=5):
+    """
+    Samples topics based on their importance or prevalanceself.
+
+    Arguments:
+        topics (list): List of (topic, score) tuples, where the score determines
+            the importance / prevalance of the topic
+        n (int): Number of topics to be sampled
+    Returns:
+        sample (list): List of randomly sampled topics
+    """
+    pass
+
+
+def compute_community_betweenness(user_user_graph, community):
+    """
+    Computes the averege betweenness of a community. This is the average of the
+    betweenness score for all the nodes in the community with respect to the
+    user-user graph. The idea being that communities that have different
+    views and interests will have higher average betweeness, since their
+    nodes will be linked to nodes in other communities.
+
+    Arguments:
+        user_user_graph (nx.Graph): User-user graph to link users to
+            other users
+        community (list): List of node (ids) in community
+
+    Returns:
+        community_betweenness (float): Average betweeness of community
+    """
+    pass
+
+
+def determine_prototype(user_user_graph, community):
+    """
+    Determines the 'prototype' node that best describes the community. This
+    node is determined to be that with the largest in-community degree / egonet.
+
+    Arguments:
+        user_user_graph (nx.Graph): User-user graph to link users to
+            other users
+        community (list): List of node (ids) in community
+
+    Returns:
+        prototype (int): ID of node that best describes the community
+    """
+    pass
