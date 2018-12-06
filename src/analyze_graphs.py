dd5d4d82bebcd42287800665f6f364dff7f0ebbc
mrlsdvd <mrlsdvd@stanford.edu>
2018-12-06 02:17:23 -0800

    Implement community_topic_evolution

--- src/analyze_graphs.py
+++ src/analyze_graphs.py
@@ -70,18 +70,18 @@ def extract_topics_from_community(user_topic_graph, community, ratio_thresh=0.5)
         # Compute ratio for each topic, if topic hasn't already been processed
         for topic in user_topics:
             if topic not in topic_counts:
-                topic_counts[topic] = 0
-            topic_counts[topic] += 1
+                topic_counts[topic] = 0.
+            topic_counts[topic] += 1.
 
     # Compute ratios for each topic and add to output set if ratio is high enough
     num_members = len(community)
     for topic in topic_counts:
-        topic_ratio = float(topic_counts[topic]) / num_members
+        topic_ratio = topic_counts[topic] / num_members
         if topic_ratio > ratio_thresh:
             topic_ratios.add((topic, topic_ratio))
 
 
-def community_topic_evolution(community_levels, user_topic_graph):
+def community_topic_evolution(community_levels, user_topic_graph, sample_n=None):
     """
     Analyzes the topic distribution in communities over levels (such as those
     returned by Girvan-Newman community detection).
@@ -92,6 +92,9 @@ def community_topic_evolution(community_levels, user_topic_graph):
             after iteration i+1 of the Girvan-Newman method
         user_topic_graph (nx.Graph): User-topic graph to link users to
             their topics
+        sample_n (int): If not None, then sample_n topics will be sampled from
+            the topic distribution of each community, as opposed to keeping
+            all topics
 
     Returns:
         evolution (list): List of tuples of list of topics, where tuple at
@@ -99,21 +102,44 @@ def community_topic_evolution(community_levels, user_topic_graph):
             distribution of topics for each community after iteration i+1 of
             the Girvan-Newman method
     """
-    pass
+    evolution = []
+    # Consider each level
+    for level in community_levels:
+        level_topics = []
+        # Consider each community in level
+        for community in level:
+            # Extract topics from community
+            topics = extract_topics_from_community(user_topic_graph, community)
+            # Sample topics if necessary
+            if sample_n is not None:
+                topics = sample_topics(topics, sample_n)
+            level_topics.append(topics)
+        evolution.append(tuple(level_topics))
+
+    return evolution
 
 
-def sample_topics(topics, n=5):
+def sample_topics(topic_scores, n=5):
     """
-    Samples topics based on their importance or prevalanceself.
+    Samples topics based on their importance or prevalance.
 
     Arguments:
-        topics (list): List of (topic, score) tuples, where the score determines
+        topic_scores (list): List of (topic, score) tuples, where the score determines
             the importance / prevalance of the topic
         n (int): Number of topics to be sampled
     Returns:
         sample (list): List of randomly sampled topics
     """
-    pass
+    topics, scores = tuple(zip(*topics))
+    # Normalize scores to sum to 1
+    scores = np.array(scores)
+    scores = scores / np.sum(scores)
+    # Number of values that will be drawn should be min(n, len(topics))
+    n = min(n, len(topics))
+
+    topics = np.random.choice(topics, size=n, replace=False, p=scores)
+    return topics.tolist()
+
 
 
 def compute_community_betweenness(user_user_graph, community):
