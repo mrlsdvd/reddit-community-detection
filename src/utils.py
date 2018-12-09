ac352f5a52cdc81d5d4f34a71579643f392f5a94
mrlsdvd <mrlsdvd@stanford.edu>
2018-12-08 20:42:40 -0800

    Add helper for creating topic map and update plotting

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,18 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
-def plot_graph(G):
-    nx.draw(G, pos=nx.spring_layout(G, k=1), alpha=.5)
+def plot_graph(G, communities=None):
+    if communities is None:
+        communities = [G.nodes()]
+    num_communities = len(communities)
+    colors = ['r', 'b', 'g', 'o'] * num_communities
+    for i in renge(num_communities):
+        community = communities[i]
+        nx.draw_networkx_nodes(G, nodelist=community, node_color=colors[i], pos=nx.spring_layout(G, k=1), alpha=.8)
+    nx.draw_networkx_edges(G, pos=nx.spring_layout(G, k=1))
     plt.show()
+
 
 def get_literal_topics(topic_nodes, topic_map):
     """
@@ -20,3 +29,24 @@ def get_literal_topics(topic_nodes, topic_map):
     for topic_node in topic_nodes:
         literal_topics.append(topic_map[topic_node])
     return literal_topics
+
+
+def create_topic_map(node_topic_path):
+    """
+    Creates topic map from tsv file
+
+    Arguments:
+        node_topic_path (str): Path to tsv file containing mapping from
+            topic literal to node id
+
+    Returns:
+        topic_map (dict): Mapping from topic node id to topic literals
+    """
+    topic_map = dict()
+    with open(node_topic_path, 'r') as node_topics:
+        for line in node_topics:
+            topic, node = node_topics.split('\t')
+            node = int(node)
+            topic_map[node] = topic
+
+    return topic_map
